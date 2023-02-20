dad9dbfebd52d75d95fa33d6ccc1c77052f29f5f
Steve Green <121233991+steveygfromao@users.noreply.github.com>
2023-02-20 08:22:46 +0000

    First capture

---
+++ SQSConsumer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SQSConsumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SQSConsumer sqsConsumer = new SQSConsumer();
+
+            await sqsConsumer.Listen();
+
+        }
+    }
+}
